10518ec1b84bcfe37e27174521fb871625cde12e
Burak Aksu <sabburak26@gmail.com>
2021-08-12 14:08:34 -0400

    parse json in express

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express()
+const path = require("path");
+
+//sets up the express app
+
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+// sets up the express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"))
